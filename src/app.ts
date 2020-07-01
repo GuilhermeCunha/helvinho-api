eb192c524009beed39da9e647d9104b9bf41cc27
GuilhermeCunha <guiscunha@gmail.com>
2020-07-01 08:48:47 -0300

    Custom Cors

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,13 @@
 // import routes from './routes'
 import * as express from 'express'
-import * as cors from 'cors'
+// import * as cors from 'cors'
 import * as moment from 'moment-timezone'
 import Routes from './routes'
 import AuthenticationsController from './controllers/AuthenticationsController'
 import AuthService from './utils/AuthService'
+import CustomCors from './utils/CustomCors'
 
 moment.tz.setDefault('America/Sao_Paulo')
-// const allowedOrigins = ['https://helvinho-web.vercel.app', 'http://localhost:3333', 'http://localhost:3000']
-
-const corsOptions: cors.CorsOptions = {
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-access-token', 'Authorization'],
-  methods: ['GET', 'DELETE', 'PUT', 'POST'],
-  // origin: function (origin, cb) {
-  //   if (!origin) return cb(null, true)
-
-  //   if (allowedOrigins.indexOf(origin) === -1) {
-  //     const msg = `A politica de CORS desta API não aceita a origem ${origin}`
-  //     return cb(new Error(msg), false)
-  //   }
-
-  //   return cb(null, true)
-  // }
-  origin: '*'
-}
 
 class App {
   public express: express.Application
@@ -37,8 +21,9 @@ class App {
   }
 
   private middlewares (): void {
+    this.express.use(CustomCors)
     this.express.use(express.json())
-    this.express.use(cors(corsOptions))
+    // this.express.use(cors(corsOptions))
   }
 
   private routes (): void {
