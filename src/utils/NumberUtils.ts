8564bab4c1c584ae47a7c592e3a5b4d1613c98c1
GuilhermeCunha <guiscunha@gmail.com>
2020-07-29 11:19:47 -0300

    Verificando se parâmetro é número

--- src/utils/NumberUtils.ts
+++ src/utils/NumberUtils.ts
@@ -1,5 +1,9 @@
 class NumberUtils {
   isNumber (value: string | number): boolean {
+    if (value === undefined) {
+      return false
+    }
+
     return ((value != null) &&
              (value !== '') &&
              !isNaN(Number(value.toString())))
